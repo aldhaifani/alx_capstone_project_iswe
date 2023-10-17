e6aeeb92f4e8aa38355ce05b0b4641ef5ef5707f
Tareq <tareqaldhaifani@gmail.com>
2023-10-17 12:29:26 +0400

    added routes for forms

--- fundtrack/views.py
+++ fundtrack/views.py
@@ -26,11 +26,31 @@ def assets():
     return render_template("assets.html")
 
 
+@views.route("/assets/add", methods=["GET", "POST"])
+@login_required
+def form_assets():
+    session["title"] = "Add assets"
+    return render_template("form_assets.html")
+
+
 @views.route("/equity_liability")
 @login_required
 def equity_liability():
     session["title"] = "Equity & Liability"
     return render_template("equity_liability.html")
+
+
+@views.route("/equity_liability/add_equity")
+@login_required
+def form_equity():
+    session["title"] = "Add Equity"
+    return render_template("form_equity.html")
+
+@views.route("/equity_liability/add_liability")
+@login_required
+def form_liability():
+    session["title"] = "Add Liability"
+    return render_template("form_liability.html")
 
 
 @views.route("/reports")
