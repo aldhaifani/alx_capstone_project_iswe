6224a88f96a91d6203f2e971d4a4d374b238e5ad
Tareq <tareqaldhaifani@gmail.com>
2023-10-18 12:00:23 +0400

    refactored the code a little

--- fundtrack/auth.py
+++ fundtrack/auth.py
@@ -1,11 +1,11 @@
 """
 authentication blueprint definition script
 """
-from flask import Blueprint, render_template, flash, request, redirect, url_for, session
-from .models import User
+from flask import Blueprint, render_template, request, redirect, url_for, session
 from werkzeug.security import generate_password_hash, check_password_hash
-from . import db
 from flask_login import login_user, login_required, logout_user, current_user
+from .models import User
+from . import db
 
 auth = Blueprint("auth", __name__)
 
@@ -14,20 +14,16 @@ auth = Blueprint("auth", __name__)
 def login():
     if request.method == "POST":
         email = request.form.get("email")
-        passwd = request.form.get("login_passwd")
+        passwd = request.form.get("passwd")
 
         user = User.query.filter_by(email=email).first()
 
-        if user:
-            if check_password_hash(user.password, passwd):
-                login_user(user, remember=True)
-                session["username"] = current_user.name
-                return redirect(url_for("views.dashboard"))
-            else:
-                flash("Please check your email and password.", category="error")
-                return render_template("login.html", user_exist=False)
-        return render_template("login.html", user_exist=False)
-    return render_template("login.html")
+        if user and check_password_hash(user.password, passwd):
+            login_user(user, remember=True)
+            session["username"] = current_user.name.split(" ")[0]
+            return redirect(url_for("views.dashboard"))
+        return render_template("login.html", user_exist="False")
+    return render_template("login.html", user_exist="True")
 
 
 @auth.route("/logout")
@@ -47,20 +43,17 @@ def signup():
         # form validations are already done in javascript
         # creating and adding a new user to the db
 
-        if db.session.query(User).filter_by(name=name).count() < 1:
-            if db.session.query(User).filter_by(email=email).count() < 1:
-                new_user = User(
-                    email=email,
-                    name=name,
-                    password=generate_password_hash(passwd, method="scrypt"),
-                )
-                db.session.add(new_user)
-                db.session.commit()
+        if db.session.query(User).filter_by(email=email).count() < 1:
+            new_user = User(
+                email=email,
+                name=name,
+                password=generate_password_hash(passwd, method="scrypt"),
+            )
+            db.session.add(new_user)
+            db.session.commit()
 
-                # redirect the user to the login page
-                return redirect(url_for("auth.login"))
-            else:
-                return render_template("signup.html", user_already_exist="True")
-        return render_template("signup.html", username_exists="True")
+            # redirect the user to the login page
+            return redirect(url_for("auth.login"))
+        return render_template("signup.html", user_already_exist="True")
 
-    return render_template("signup.html")
+    return render_template("signup.html", user_already_exist="False")
