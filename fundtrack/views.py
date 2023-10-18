d9ce2226151faf45f8aebf58151566df7f1a2a44
Tareq <tareqaldhaifani@gmail.com>
2023-10-18 21:14:01 +0400

    defined routes for adding and deleting instances from the db

--- fundtrack/views.py
+++ fundtrack/views.py
@@ -1,10 +1,19 @@
 """
 routes blueprint definition script
 """
-from flask import Blueprint, render_template, session, request, redirect, url_for
+from flask import (
+    Blueprint,
+    render_template,
+    session,
+    request,
+    redirect,
+    url_for,
+    jsonify,
+)
 from flask_login import login_required, current_user
 from .models import Asset, Equity, Liability
 from . import db
+import json
 
 views = Blueprint("views", __name__)
 
@@ -45,7 +54,7 @@ def assets():
 
 @views.route("/assets/add", methods=["GET", "POST"])
 @login_required
-def form_assets():
+def form_add_assets():
     """Assets add form route
 
     Returns:
@@ -67,6 +76,47 @@ def form_assets():
         return redirect(url_for("views.assets"))
 
     return render_template("form_assets.html", user=current_user)
+
+
+@views.route("/assets/edit/<string:id>", methods=["GET", "POST"])
+@login_required
+def form_edit_assets(id):
+    """Assets edit form route
+
+    Returns:
+        renter_template or redirect
+    """
+    session["title"] = "Edit assets"
+
+    asset = Asset.query.get(id)
+
+    if request.method == "POST":
+        asset.name = request.form.get("name")
+        asset.value = int(request.form.get("value"))
+        asset.current = True if request.form.get("asset_type") == "current" else False
+
+        db.session.commit()
+
+        if asset.id == 1:
+            session["cash"] = asset.value
+
+        return redirect(url_for("views.assets"))
+
+    return render_template("form_edit_assets.html", asset=asset)
+
+
+@views.route("/delete-asset", methods=["POST"])
+@login_required
+def delete_asset():
+    asset = json.loads(request.data)
+    assetId = asset["assetId"]
+
+    asset = Asset.query.get(assetId)
+    if asset:
+        if asset.user_id == current_user.id:
+            db.session.delete(asset)
+            db.session.commit()
+    return jsonify({})
 
 
 @views.route("/equity_liability")
@@ -104,6 +154,43 @@ def form_equity():
     return render_template("form_equity.html", user=current_user)
 
 
+@views.route("/equity_liability/edit_equity/<string:id>", methods=["GET", "POST"])
+@login_required
+def form_edit_equity(id):
+    """Equity edit form route
+
+    Returns:
+        renter_template or redirect
+    """
+    session["title"] = "Edit equity"
+
+    equity = Equity.query.get(id)
+
+    if request.method == "POST":
+        equity.name = request.form.get("name")
+        equity.value = int(request.form.get("value"))
+
+        db.session.commit()
+
+        return redirect(url_for("views.equity_liability"))
+
+    return render_template("form_edit_equity.html", equity=equity)
+
+
+@views.route("/delete-equity", methods=["POST"])
+@login_required
+def delete_equity():
+    equity = json.loads(request.data)
+    equityId = equity["equityId"]
+
+    equity = Equity.query.get(equityId)
+    if equity:
+        if equity.user_id == current_user.id:
+            db.session.delete(equity)
+            db.session.commit()
+    return jsonify({})
+
+
 @views.route("/equity_liability/add_liability", methods=["GET", "POST"])
 @login_required
 def form_liability():
@@ -126,17 +213,49 @@ def form_liability():
         )
         db.session.add(new_liability)
         db.session.commit()
-
-        for liability in current_user.liabilities:
-            print(
-                "{} -- {} -- {}".format(
-                    liability.name, liability.value, liability.current
-                )
-            )
-
         return redirect(url_for("views.equity_liability"))
 
     return render_template("form_liability.html", user=current_user)
+
+
+@views.route("/equity_liability/edit_liability/<string:id>", methods=["GET", "POST"])
+@login_required
+def form_edit_liability(id):
+    """Liability edit form route
+
+    Returns:
+        renter_template or redirect
+    """
+    session["title"] = "Edit liability"
+
+    liability = Liability.query.get(id)
+
+    if request.method == "POST":
+        liability.name = request.form.get("name")
+        liability.value = int(request.form.get("value"))
+        liability.current = (
+            True if request.form.get("asset_type") == "current" else False
+        )
+
+        db.session.commit()
+
+        return redirect(url_for("views.equity_liability"))
+
+    return render_template("form_edit_liability.html", liability=liability)
+
+
+@views.route("/delete-liability", methods=["POST"])
+@login_required
+def delete_liability():
+    liability = json.loads(request.data)
+    liabilityId = liability["liabilityId"]
+
+    liability = Liability.query.get(liabilityId)
+    if liability:
+        if liability.user_id == current_user.id:
+            db.session.delete(liability)
+            db.session.commit()
+    return jsonify({})
 
 
 @views.route("/reports")
