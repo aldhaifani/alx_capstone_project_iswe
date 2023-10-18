4d90fd2c2d1449cb0bce6d8aea084e098055f4f2
Tareq <tareqaldhaifani@gmail.com>
2023-10-18 10:56:14 +0400

    defined add form routes, docstr, style formatting

--- fundtrack/views.py
+++ fundtrack/views.py
@@ -1,60 +1,151 @@
 """
 routes blueprint definition script
 """
-from flask import Blueprint, render_template, session
+from flask import Blueprint, render_template, session, request, redirect, url_for
 from flask_login import login_required, current_user
+from .models import Asset, Equity, Liability
+from . import db
 
 views = Blueprint("views", __name__)
 
 
 @views.route("/")
 def home():
+    """Home route
+
+    Returns:
+        renter_template
+    """
     return render_template("home.html")
 
 
 @views.route("/dashboard")
 @login_required
 def dashboard():
+    """Dashboard route
+
+    Returns:
+        renter_template
+    """
     session["title"] = "Dashboard"
-    return render_template("dashboard.html")
+    return render_template("dashboard.html", user=current_user)
 
 
 @views.route("/assets")
 @login_required
 def assets():
+    """Assets route
+
+    Returns:
+        _type_: _description_
+    """
     session["title"] = "Assets"
-    return render_template("assets.html")
+    return render_template("assets.html", user=current_user)
 
 
 @views.route("/assets/add", methods=["GET", "POST"])
 @login_required
 def form_assets():
+    """Assets add form route
+
+    Returns:
+        renter_template or redirect
+    """
     session["title"] = "Add assets"
-    return render_template("form_assets.html")
+
+    if request.method == "POST":
+        name = request.form.get("name")
+        value = int(request.form.get("value"))
+        asset_type = True if request.form.get("asset_type") == "current" else False
+
+        new_asset = Asset(
+            name=name, value=value, current=asset_type, user_id=current_user.id
+        )
+        db.session.add(new_asset)
+        db.session.commit()
+
+        return redirect(url_for("views.assets"))
+
+    return render_template("form_assets.html", user=current_user)
 
 
 @views.route("/equity_liability")
 @login_required
 def equity_liability():
+    """equity_liability add form route
+
+    Returns:
+        renter_template
+    """
     session["title"] = "Equity & Liability"
-    return render_template("equity_liability.html")
+    return render_template("equity_liability.html", user=current_user)
 
 
-@views.route("/equity_liability/add_equity")
+@views.route("/equity_liability/add_equity", methods=["GET", "POST"])
 @login_required
 def form_equity():
-    session["title"] = "Add Equity"
-    return render_template("form_equity.html")
+    """Equity add form route
 
-@views.route("/equity_liability/add_liability")
+    Returns:
+        renter_template or redirect
+    """
+    session["title"] = "Add Equity"
+
+    if request.method == "POST":
+        name = request.form.get("name")
+        value = int(request.form.get("value"))
+
+        new_equity = Equity(name=name, value=value, user_id=current_user.id)
+        db.session.add(new_equity)
+        db.session.commit()
+
+        return redirect(url_for("views.equity_liability"))
+
+    return render_template("form_equity.html", user=current_user)
+
+
+@views.route("/equity_liability/add_liability", methods=["GET", "POST"])
 @login_required
 def form_liability():
+    """Liability add form route
+
+    Returns:
+        renter_template or redirect
+    """
     session["title"] = "Add Liability"
-    return render_template("form_liability.html")
+
+    if request.method == "POST":
+        name = request.form.get("name")
+        value = int(request.form.get("value"))
+        liability_type = (
+            True if request.form.get("liability_type") == "current" else False
+        )
+
+        new_liability = Liability(
+            name=name, value=value, current=liability_type, user_id=current_user.id
+        )
+        db.session.add(new_liability)
+        db.session.commit()
+
+        for liability in current_user.liabilities:
+            print(
+                "{} -- {} -- {}".format(
+                    liability.name, liability.value, liability.current
+                )
+            )
+
+        return redirect(url_for("views.equity_liability"))
+
+    return render_template("form_liability.html", user=current_user)
 
 
 @views.route("/reports")
 @login_required
 def reports():
+    """reports add form route
+
+    Returns:
+        renter_template
+    """
     session["title"] = "Reports"
-    return render_template("reports.html")
+    return render_template("reports.html", user=current_user)
